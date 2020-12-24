a19492e243eb8db20ba5b816309863b385f4e2c8
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2020-12-25 01:24:25 +0600

    add script.js

---
+++ js/script.js
@@ -0,0 +1,67 @@
+var noOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < noOfDrumButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        makeDrumSound (this.innerText);
+        buttonAnimation (this.innerText);
+    });
+}
+
+document.addEventListener("keypress", function (event) {
+    makeDrumSound (event.key);
+    buttonAnimation (event.key);
+});
+
+function makeDrumSound (value) {
+    switch (value) {
+        case 'a':
+            var crash = new Audio ("sounds/crash.mp3");
+            crash.play();               
+            break;
+
+        case 's':
+            var kickBass = new Audio ("sounds/kick-bass.mp3");
+            kickBass.play();               
+            break;
+
+        case 'd':
+            var snare = new Audio ("sounds/snare.mp3");
+            snare.play();               
+            break;
+
+        case 'f':
+            var tom1 = new Audio ("sounds/tom-1.mp3");
+            tom1.play();               
+            break;
+
+        case 'j':
+            var tom2 = new Audio ("sounds/tom-2.mp3");
+            tom2.play();               
+            break;
+
+        case 'k':
+            var tom3 = new Audio ("sounds/tom-3.mp3");
+            tom3.play();               
+            break;
+
+        case 'l':
+            var tom4 = new Audio ("sounds/tom-4.mp3");
+            tom4.play();               
+            break;
+    
+        default:
+            console.log(this.innerText);
+            break;
+    }
+}
+
+function buttonAnimation (currentValue) {
+    var currentButton = document.querySelector("." + currentValue);
+
+    currentButton.classList.add("pressed");
+
+    setTimeout (function () {
+        currentButton.classList.remove("pressed");
+    }, 200);
+}
+
